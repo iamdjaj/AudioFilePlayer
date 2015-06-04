d6398a6c71b896719b95411055e198e42317dd0d
iamdjaj <ajalmanzor@gmail.com>
2015-06-04 16:06:17 -0400

    first commit

---
+++ src/audiofileplayer/AudioFilePlayer.java
@@ -0,0 +1,53 @@
+package audiofileplayer;
+
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.sound.sampled.*;
+ 
+public class AudioFilePlayer {
+	
+	public static void main(String[] args) {
+		AudioInputStream din = null;
+		try {
+			// File file = new File(args[0]);
+			File file = new File("C:/Users/ajalmanzor/workspace2/AudioFilePlayer/src/audiofileplayer/Nicki_Minaj_Anaconda.mp3");
+			AudioInputStream in = AudioSystem.getAudioInputStream(file);
+			AudioFormat baseFormat = in.getFormat();
+			AudioFormat decodedFormat = new AudioFormat(
+					AudioFormat.Encoding.PCM_SIGNED,
+					baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+					baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
+					false);
+			din = AudioSystem.getAudioInputStream(decodedFormat, in);
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, decodedFormat);
+			SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+			if(line != null) {
+				line.open(decodedFormat);
+				byte[] data = new byte[4096];
+				// Start
+				line.start();
+				
+				int nBytesRead;
+				while ((nBytesRead = din.read(data, 0, data.length)) != -1) {	
+					line.write(data, 0, nBytesRead);
+				}
+				// Stop
+				line.drain();
+				line.stop();
+				line.close();
+				din.close();
+			}
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(din != null) {
+				try { din.close(); } catch(IOException e) { }
+			}
+		}
+	}
+ 
+}
